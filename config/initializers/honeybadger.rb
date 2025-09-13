dad8c017d1476464c9f91cdbfc43b3d3ba4592df
dhiralpuab <dhiralp@uab.edu>
2025-09-13 15:57:45 -0500

    Remove Honey badger

--- config/initializers/honeybadger.rb
+++
@@ -1,8 +0,0 @@
-Honeybadger.configure do |config|
-    config.api_key = ENV['HONEYBADGER_API_KEY']
-    config.env = Rails.env
-
-    if Rails.env.development?
-        config.development_environments = []
-    end
-end
